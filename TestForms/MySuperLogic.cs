12fb040ffa0d3ec99200b692afc0ea52fff5b159
Pavel <kulbida@hotmail.com>
2020-03-20 17:30:03 +0600

    init

---
+++ TestForms/MySuperLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForms
+{
+    class MySuperLogic
+    {
+        public void Msg()
+        {
+            MessageBox.Show("Hello");
+        }
+
+        public DateTime GetDate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
